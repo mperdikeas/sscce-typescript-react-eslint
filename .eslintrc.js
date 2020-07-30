d165d811fc9abaa7cda1c9b639bc248208885392
mperdikeas <mperdikeas@gmail.com>
2020-07-30 13:08:57 +0300

    cosmetic

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,23 @@
-module.exports = {
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
-  parserOptions: {
-    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-    ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
-  },
-  settings: {
-    react: {
-      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
-  },
-  extends: [
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
-  ],
-  rules: {
-    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-    // e.g. "@typescript-eslint/explicit-function-return-type": "off",
-  },
-};
+    module.exports = {
+        parser: "@typescript-eslint/parser",
+        "plugins": ["@typescript-eslint"],
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true
+        }
+      },
+      settings: {
+        react: {
+          version: "detect"
+        }
+      },
+      extends: [
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended"
+      ],
+      rules: {
+          "extends": ["eslint:recommended", "plugin:@typescript-eslint/recommended"]
+      },
+    };
